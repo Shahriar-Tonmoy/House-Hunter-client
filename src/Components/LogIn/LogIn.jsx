db730fdeb71a6c07fbc2ac77ad77b80dafae9893
Tonmoy <shahariartonmoy17@gmail.com>
2024-01-23 21:39:41 +0600

    Reistration done

---
+++ src/Components/LogIn/LogIn.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+
+const LogIn = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200 bg-gradient-to-r from-[#6B240C] to-[#994D1C]">
+      <ToastContainer></ToastContainer>
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left"></div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-[#F5CCA0] py-20 px-5">
+          <h1 className="text-[#6B240C] text-3xl font-bold text-center">
+            Registration
+          </h1>
+          <form className="card-body" onSubmit={handleNewUser}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#6B240C] font-bold">
+                  Name
+                </span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered focus:outline-none text-[#6B240C] font-semibold bg-[#E48F45]"
+                name="name"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#6B240C] font-bold">
+                  Role
+                </span>
+              </label>
+              <select
+                name="role"
+                className="select select-info border-none focus:outline-none w-full bg-[#E48F45] text-[#6B240C] font-semibold  max-w-xs"
+              >
+                <option className="bg-orange-300 text-[#6B240C] font-semibold">
+                  House Owner
+                </option>
+                <option className="bg-orange-100 text-[#6B240C] font-semibold">
+                  House Renter
+                </option>
+              </select>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#6B240C] font-bold">
+                  Phone Number
+                </span>
+              </label>
+              <input
+                type="tel"
+                className="input input-bordered focus:outline-none text-[#6B240C] font-semibold bg-[#E48F45]"
+                name="phone"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#6B240C] font-bold">
+                  Email
+                </span>
+              </label>
+              <input
+                type="email"
+                className="input input-bordered focus:outline-none text-[#6B240C] font-semibold bg-[#E48F45]"
+                name="email"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-[#6B240C] font-bold">
+                  Password
+                </span>
+              </label>
+              <input
+                type="password"
+                className="input input-bordered focus:outline-none text-[#6B240C] font-semibold bg-[#E48F45]"
+                name="password"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-opacity-0 font-bold text-[#6B240C] border-[#6B240C] hover:bg-opacity-0 hover:border-[#6B240C]">
+                Sign up
+              </button>
+            </div>
+          </form>
+          {/* <p className="text-center">Already have an account? <Link className="text-[#00ADB5] underline" to="/SignIn">Sign In</Link> here</p> */}
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default LogIn;
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Registration from './Components/Registration/Registration.jsx';
+import Root from './Components/Root/Root.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root>,
+    children:[
+      {
+        path:"/registration",
+        element:<Registration></Registration>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
